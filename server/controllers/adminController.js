68168e2f4be0910b96d4cc1a27f6c7b8f7ed1863
NIKET RAJ <147556278+Niketraj08@users.noreply.github.com>
2026-01-03 19:39:29 +0530

    Update adminController.js error solve

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -25,7 +25,6 @@ const getDashboardStats = async (req, res) => {
     }
 };
 
-// @desc    Get all users
 const getUsers = async (req, res) => {
     try {
         const users = await User.find({});
